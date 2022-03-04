5888bafa390d565a761c52126f760ae03a4a5fa1
jnoori31 <jonasgeneralassembly@gmail.com>
2022-03-04 09:37:45 +0000

    models and routes update

--- application/routes.py
+++ application/routes.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from application import app, db
 from application.models import Recipe, User
-from flask import request
+from flask import render_template, request
 
 #<.....................HOME PAGE.................>
 @app.route('/', methods=['GET'])
@@ -11,3 +11,8 @@ def home_page():
 #<.......................Logic for User Login............................>
 
 #<>>>>>>>>>>>>>>>>>>>>>C-R-U-D for Recipe...............>
+@app.route('/', methods=['GET'])
+@app.route('/read', methods=['GET'])
+def read():
+    recipes = Recipe.query.all()
+    return render_template('read.html', recipe=recipes)
