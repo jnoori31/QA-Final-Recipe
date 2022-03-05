dd8b28d697f444e6ee34f3afbe6bec310e8f53d4
jnoori31 <jonasgeneralassembly@gmail.com>
2022-03-05 17:13:17 +0000

    login_manger function created in routes

--- application/models.py
+++ application/models.py
@@ -1,7 +1,11 @@
-from application import db
+from application import db, login_manager
 from application import bcrypt
 from flask_login import UserMixin
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+    
 class User(db.Model, UserMixin):
     # Define columns for user data
     id = db.Column(db.Integer, primary_key = True, autoincrement = True) # Primary key
