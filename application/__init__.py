4ab43586296a21920a67f609b328c6d03123a33f
jnoori31 <jonasgeneralassembly@gmail.com>
2022-03-09 21:11:49 +0000

    Epic3 docker swarm

--- application/__init__.py
+++ application/__init__.py
@@ -6,11 +6,11 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://qauser:newpassword123@local/qa_db"
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://qauser:newpassword123@local/qa_db"
 #app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.db"
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URI")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.config['SECRET_KEY'] = 'hreuifh7ueyhf'
+app.config['SECRET_KEY'] = 'hreuifh7ueyhf'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
