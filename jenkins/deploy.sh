40bd17c450140b837bccd31b56f1b9fe625938d5
jnoori31 <jonasgeneralassembly@gmail.com>
2022-03-20 07:32:45 +0000

    refactor yaml to yml

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -4,9 +4,9 @@ echo "Deploy stage"
 
 # -o flag, as we are sshing to server for first time
 scp -o 'StrictHostKeyChecking no' \
-    docker-stack.yaml \
-    jenkins@swarm-deployment-server.uksouth.cloudapp.azure.com:/home/jenkins/docker-stack.yaml
+    docker-stack.yml \
+    jenkins@swarm-deployment-server.uksouth.cloudapp.azure.com:/home/jenkins/docker-stack.yml
 
 ssh -o 'StrictHostKeyChecking no' \
     jenkins@swarm-deployment-server.uksouth.cloudapp.azure.com \
-    'docker stack deploy --compose-file docker-stack.yaml qa-recipe'
+    'docker stack deploy --compose-file docker-stack.yml qa-recipe'
