016de9760e39b54c5f020196fe09b49c7ce55af7
jnoori31 <jonasgeneralassembly@gmail.com>
2022-03-20 07:21:15 +0000

    add deployment stage to pipeline

--- jenkins/deploy.sh
+++ jenkins/deploy.sh
@@ -2,5 +2,11 @@
 
 echo "Deploy stage"
 
-scp docker-stack.yaml jenkins@swarm-deployment-server.uksouth.cloudapp.azure.com:/home/jenkins/docker-stack.yaml
-ssh jenkins@swarm-deployment-server.uksouth.cloudapp.azure.com 'docker stack deploy --compose-file docker-stack.yaml qa-recipe'
+# -o flag, as we are sshing to server for first time
+scp -o 'StrictHostKeyChecking no' \
+    docker-stack.yaml \
+    jenkins@swarm-deployment-server.uksouth.cloudapp.azure.com:/home/jenkins/docker-stack.yaml
+
+ssh -o 'StrictHostKeyChecking no' \
+    jenkins@swarm-deployment-server.uksouth.cloudapp.azure.com \
+    'docker stack deploy --compose-file docker-stack.yaml qa-recipe'
