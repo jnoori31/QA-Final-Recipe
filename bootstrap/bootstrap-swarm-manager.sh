6c30acfd7d15552e77e233e527b0238e1da3fe73
jnoori31 <jonasgeneralassembly@gmail.com>
2022-03-20 07:04:19 +0000

    update setup scripts

--- bootstrap/bootstrap-swarm-manager.sh
+++ bootstrap/bootstrap-swarm-manager.sh
@@ -3,26 +3,25 @@ echo "Setup stage"
 
 # apt dependencies
 sudo apt-get update
-sudo apt-get install -y curl jq python3-venv
+sudo apt-get install \
+    ca-certificates \
+    gnupg \
+    lsb-release
 
 # install docker: https://docs.docker.com/engine/install/ubuntu/
-if [ ! -f "/usr/bin/docker" ]; then
     sudo apt-get remove docker docker-engine docker.io containerd runc
-    sudo apt-get install \
-        ca-certificates \
-        gnupg \
-        lsb-release
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
     
     echo \
         "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null}
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
+    
     sudo apt-get update
     sudo apt-get install docker-ce docker-ce-cli containerd.io
 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
-fi
 # creating a jenkins user and giving it permission to run docker
 sudo usermod -aG docker jenkins || true
 
@@ -31,6 +30,6 @@ version=$(curl -s https://api.github.com/repos/docker/compose/releases/latest | 
 sudo curl -L "https://github.com/docker/compose/releases/download/${version}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-systemctl restart docker
+sudo systemctl restart docker
 docker swarm init
 docker node ls
--- jenkins/setup.sh
+++ jenkins/setup.sh
@@ -9,19 +9,19 @@ sudo apt-get install -y curl jq python3-venv
 # install docker: https://docs.docker.com/engine/install/ubuntu/
 if [ ! -f "/usr/bin/docker" ]; then
     sudo apt-get remove docker docker-engine docker.io containerd runc
-    sudo apt-get install \
+    sudo apt-get install -y \
         ca-certificates \
         gnupg \
         lsb-release
     
     echo \
         "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null}
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
     sudo apt-get update
-    sudo apt-get install docker-ce docker-ce-cli containerd.io
+    sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor --yes -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 fi
 # creating a jenkins user and giving it permission to run docker
