36e63ba7b15c43bda4a032f3366271ed8234823a
jnoori31 <jonasgeneralassembly@gmail.com>
2022-03-24 08:44:01 +0000

    refactored build stage to use secrets

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-TAG=${BUILD_ID}
-DB_USER=${DB_LOGIN_USR}
-DB_PASSWORD=${DB_LOGIN_PSW}
-DB_ROOT_PASSWORD=${DB_ROOT_PSW}
+export TAG=${BUILD_ID}
+export DB_USER=${DB_LOGIN_USR}
+export DB_PASSWORD=${DB_LOGIN_PSW}
+export DB_ROOT_PASSWORD=${DB_ROOT_PSW}
 
 env
 docker-compose -f docker-stack.yml build
